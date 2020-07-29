6d51801af2e141ff59d5aafdfce5f6cbc56e429f
keerthana7397 <59758932+keerthana7397@users.noreply.github.com>
2020-07-29 16:18:05 +0530

    Mini Project Q3 answer uploaded

---
+++ Mini_Project/Random_Quote.js
@@ -0,0 +1,18 @@
+var x=document.getElementsByTagName('div');
+const button = document.querySelector('#btn');
+
+button.onclick=async function fetchJokes(){
+    const response = await fetch('https://type.fit/api/quotes');
+    const data = await response.json();
+
+    //Calculate random index number to display random quote wrt the index
+    let i=Math.ceil(Math.random()*(1644));
+
+    //Clear any pre-existing quote
+    x[0].innerHTML='';
+    var res=data[i-1].text;
+
+    //Display the fetched random quote
+    x[0].innerHTML+=res;    
+}
+//fetchJokes();
